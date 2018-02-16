47b91a7a0846334b340bc9a7e228a38acd844c1b
Patrick Yee <g0th1cazn@gmail.com>
2018-02-16 03:00:48 -0700

    Initial commit

---
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -0,0 +1,119 @@
+﻿var CommentBox = React.createClass({
+    loadComentsFromServer: function () {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        }.bind(this);
+        xhr.send();
+    },
+    handleCommentSubmit: function (comment) {
+        // TODO: submit to the server and refresh the list
+        var comments = this.state.data;
+        comment.id = Date.now();
+        var newComments = comments.concat([comment]);
+        this.setState({ data: newComments });
+
+        var data = new FormData();
+        data.append('Author', comment.Author);
+        data.append('Text', comment.Text);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', this.props.submitUrl, true);
+        xhr.onload = function () {
+            this.loadComentsFromServer();
+        }.bind(this);
+        xhr.send(data);
+    },
+    getInitialState: function () {
+        return { data: [] };
+    },
+    componentWillMount: function () {
+        this.loadComentsFromServer();
+        window.setInterval(this.loadComentsFromServer, this.props.pollInterval);
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+                <CommentList data={this.state.data} />
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    getInitialState: function () {
+        return { author: '', text: '' };
+    },
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) {
+            return;
+        }
+        // TODO: send request to the server
+        this.props.onCommentSubmit({ Author: author, Text: text });
+        this.setState({ author: '', text: '' });
+    },
+    render: function () {
+        return (
+            <form className="commentForm" onSubmit={this.handleSubmit} >
+                <input type="text" placeholder="Name" value={this.state.author} onChange={this.handleAuthorChange} />
+                <br />
+                <textarea type="text" rows="4" cols="50" placeholder="Comment" value={this.state.text} onChange={this.handleTextChange} />
+                <br/>
+                <input type="submit" value="Post" />
+            </form>
+            );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return (
+                <Comment author={comment.Author} key={comment.Id}>
+                    {comment.Text}
+                </Comment>
+                );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+            );
+    }
+});
+
+var Comment = React.createClass({
+    rawMarkup: function () {
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
+    render: function () {
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
+    document.getElementById('content')
+);
